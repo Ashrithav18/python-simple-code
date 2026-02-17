eaae51666f9d9c5d2518769e2e29ec34a74a4b56
Ashritha <ashrithav11@gmail.com>
2026-02-17 07:23:08 +0530

    simple code for pattern no.7

---
+++ pattern7.py
@@ -0,0 +1,13 @@
+for j in range(0,6):
+    for s in range(0,6-j-1):
+        print("",end=" ")
+    for i in range(0,j+1):
+        print("*",end=" ")
+    print("",end="\n")
+for k in range(0,5):
+    for l in range(0,k+1):
+        print("",end=" ")
+    for m in range(0,5-k):
+        print("*",end=" ")
+    print("",end="\n")
+    
