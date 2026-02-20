1f898f4d3d44f5dc13d3a566a64abcb274555458
Ashritha <ashrithav11@gmail.com>
2026-02-20 07:34:24 +0530

    Simple python code to find largest number among three

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+largest=a
+if b>largest:
+    largest=b
+if c>largest:
+    largest=c
+print("Largest among three is:",largest)
