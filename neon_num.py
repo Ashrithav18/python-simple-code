bb31ae1a7ee8cd22c0b3075e58cd73b6b01694b1
Ashritha <ashrithav11@gmail.com>
2026-02-25 14:24:25 +0530

    Python code to check whether the given number is Neon number or not

---
+++ neon_num.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a number: "))
+square=num*num
+sum_digits=0
+while square>0:
+    sum_digits+=square%10
+    square//=10
+if sum_digits==num:
+    print("Neon Numbers")
+else:
+    print("Not Neon numbers")
+    
